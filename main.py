144904a4b836cf0b76913ad946335f42d1b971d6
smyanj <smyanjaip@gmail.com>
2023-08-20 17:16:15 -0700

    Added suggestion bot based off words per min and total words in book

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     #print(text)
     #begin report
     book_report = report(text)
-
+    print(book_report)
     #num_words = count_words(text)
     #print(num_words)
     
@@ -45,7 +45,30 @@ def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
 
+def estimated_time_to_read(book):
+    wpm = int(input("What is your words per min: "))
+    
+    total_time = count_words(book) // wpm + 1
 
+    if(total_time == 0):
+        total_time = 1
+    elif(total_time > 60):
+        return (total_time // 60)
+    #return str(total_time) + 'min'
+
+
+def is_this_for_you(report):
+    #My own preferences
+    """
+    Book has to be less than 10,000 words
+    
+    Book has to take less than 50 hours to read
+    """
+    total_est = estimated_time_to_read(report)
+    if(count_words(report) >= 10000 and total_est <= 50):
+        return f"This would be a great book for you to read because it is more than 10,000 words and takes {total_est} hours to read"
+    else:
+        return f"This would not be a good book for you to read because it is {count_words(report)} words long and takes {total_est} hours to read, well above your preferred time!"
 
 def report(book):
     print(f"-- Report of books/frankenstein.txt --")
@@ -64,7 +87,8 @@ def report(book):
 
 
     #print(total_letters)
-
+    print("-- Final Thoughts --\n")
+    print(is_this_for_you(book))
     print("-- End report --")
 
 
